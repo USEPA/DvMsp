fee66466fdfad9e8bcabb87dbc2a3664457e8976
Michael Dumelle <mjdumelle@gmail.com>
2021-05-21 06:32:19 -0700

    added simulation skeleton (for parallel)

---
+++ inst/scripts/simulation1_skeleton.R
@@ -0,0 +1,29 @@
+# load packages (not needed if everything contained in the function)
+library(spsurvey)
+library(sptotal)
+
+# create simulation function 1
+sim1 <- function(seed, ...) { # dots are placeholders for other arguments
+
+  # simulate data
+
+  # fit models
+
+  # make predictions (sptotal)
+
+  # return relevant output
+}
+
+# parallel
+library(parallel) # load parallel
+n_cluster <- detectCores() # find cores (48 on mine)
+cluster <- makeCluster(n_cluster) # make cluster
+clusterEvalQ(cluster, library(spsurvey)) # export spsurvey to cluster
+clusterEvalQ(cluster, library(sptotal)) # export sptotal to cluster
+sim1_output <- parLapply(cluster, seed, sim1, ...) # could initially use safely(sim1)
+stopCluster(cluster) # stop cluster
+
+# model summaries
+library(tidyverse)
+
+# do summaries
