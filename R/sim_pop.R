68601c97843159c5e522d437414be759a89256b0
Matt Higham <highamm@oregonstate.edu>
2021-06-08 12:54:49 -0700

    added roxygen to sim_pop.

--- R/sim_pop.R
+++ R/sim_pop.R
@@ -1,3 +1,29 @@
+#' Simulate spatially correlated data
+#'
+#' simulates a spatially correlated response on either a gridded or
+#' random unit square. The resulting data can then be used in
+#' \code{\link{sim_one}()}.
+#'
+#' @param N is the total number of data points. For \code{gridded = TRUE},
+#' this must be a perfect square.
+#' @param n is number of data points sampled.
+#' @param gridded \code{TRUE} for gridded sites and \code{FALSE} for
+#' points with random locations.
+#' @param cortype is the correlation function, which is
+#' \code{"Exponential"}
+#' @param psill is the partial sill.
+#' @param erange is the effective range.
+#' @param nugget is the nugget.
+#' @param ... further arguments passed to or from other methods.
+#' @return a data frame with \itemize{
+#'   \item \code{x}, a column with the spatial x-coordinates.
+#'   \item \code{y}, a column with the spatial y-coordinates.
+#'   \item \code{response}, a column with the response variable.
+#' }
+#' @examples
+#' sim_pop(N = 100, n = 50, gridded = TRUE, cortype = "Exponential", psill = 1, erange = 1, nugget = 0.2)
+#' @export
+
 ## simulate population
 # need spsurvey and sptotal and dplyr
 sim_pop <- function(N = 100, n = 50, gridded = TRUE, cortype, psill, erange, nugget, ...) {
@@ -9,7 +35,7 @@ sim_pop <- function(N = 100, n = 50, gridded = TRUE, cortype, psill, erange, nug
     }
     ## simulate grid
     sqrN <- ceiling(sqrt(N))
-    N <- sqrN^2
+    N <- sqrN ^ 2
     x <- seq(from = 0, to = 1, length.out = sqrN)
     y <- seq(from = 0, to = 1, length.out = sqrN)
     data <- expand.grid(x = x, y = y)
@@ -32,11 +58,10 @@ sim_pop <- function(N = 100, n = 50, gridded = TRUE, cortype, psill, erange, nug
   data$response <- as.vector(t(chol_covmx) %*% rnorm(N))
 
   ## return data
-  data
+  return(data)
 }
 
 covmx_exp <- function(distmx, psill, erange, nugget) {
   psill * exp(-3 * distmx / erange) + nugget * (distmx == 0)
 }
 
-# sim_pop(N = 100, n = 50, gridded = TRUE, cortype = "exponential", psill = 1, erange = 1, nugget = 0.2)
