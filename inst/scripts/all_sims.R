f0ea695f5256bdb09472dc210f6db5ae78485627
Matt Higham <highamm@oregonstate.edu>
2021-09-14 09:20:56 -0400

    Updated simulations to include 48 settings and be run in a single .r file.

---
+++ inst/scripts/all_sims.R
@@ -0,0 +1,99 @@
+library(tidyverse)
+library(parallel)
+
+# write results?
+write_out <- TRUE
+
+## set up grid for simulation settings
+N <- 30 ^ 2
+n <- c(10, 50, 100, 200)
+total_var <- 2
+psill_ratio <- c(0, 0.5, 0.9)
+erange <- sqrt(4)
+gridded <- c(TRUE, FALSE)
+resptype <- c("normal", "lognormal")
+
+parm_df <- expand_grid(N, n, total_var, psill_ratio,
+                       erange, gridded, resptype) %>%
+  mutate(nugget_ratio = 1 - psill_ratio,
+         psill = total_var * psill_ratio,
+         nugget = total_var * nugget_ratio)
+
+# source functions for now
+source("R/sim_pop.R")
+source("R/sim_trial.R")
+
+## loop through each row of parm_df
+for (i in 1:nrow(parm_df)) {
+  n_trials <- 5
+  seed <- runif(n_trials, 1, 1e7)
+  n_cluster <- detectCores() # find cores (48 on mine)
+  cluster <- makeCluster(n_cluster) # make cluster
+  clusterExport(cluster, varlist = c("sim_pop", "covmx_exp"))
+  clusterEvalQ(cluster, library(spsurvey)) # export spsurvey to cluster
+  clusterEvalQ(cluster, library(sptotal)) # export sptotal to cluster
+  clusterEvalQ(cluster, library(dplyr)) # export sptotal to cluster
+
+  parm_df_sim <- parm_df %>% slice(i) ## grab row for ith parameter combo
+  sim_output <- parLapply(
+    cluster, # the cluster
+    seed, # some seeds
+    safely(sim_trial), # see if any errors
+    ## pull each parameter from the single row data frame
+    N = parm_df_sim %>% pull(N), # pop size
+    n = parm_df_sim %>% pull(n), # sample size
+    psill = parm_df_sim %>% pull(psill), # partial sill
+    nugget = parm_df_sim %>% pull(nugget), # nugget
+    erange = parm_df_sim %>% pull(erange), # effective range
+    gridded = parm_df_sim %>% pull(gridded), # grid (TRUE) or random (FALSE) both in [0, 1] x [0,1]
+    resptype = parm_df_sim %>% pull(resptype) # normal response
+  )
+
+  stopCluster(cluster) # stop cluster
+
+  # check errors
+  map(sim_output, "error") %>%
+    bind_rows() %>%
+    nrow()
+
+  # do summaries
+  safe_output <- map(sim_output, "result") %>%
+    bind_rows()
+
+  # write output
+  all_output <- safe_output %>%
+    mutate(
+      resid = true_mean - estimate,
+      cover = 1 * (lb <= true_mean & true_mean <= ub)
+    ) %>%
+    group_by(seed) %>%
+    mutate(closer = case_when(
+      resid == min(resid) ~ 1,
+      resid != min(resid) ~ 0
+    )) %>%
+    mutate(parm_df %>% slice(i)) ## add parameters to data frame
+
+  # write summaries
+  summ_output <- all_output %>%
+    group_by(approach) %>%
+    summarize(
+      mpbias = mean(resid),
+      rmspe = sqrt(mean(resid^2)),
+      medae = median(abs(resid)),
+      coverage = mean(cover),
+      prcloser = mean(closer),
+      med_ci_len = median(ub - lb)
+    ) %>%
+    mutate(parm_df %>% slice(i)) ## add parameters to data frame
+
+
+  if (write_out == TRUE) {
+    library(readr)
+    write_csv(all_output, glue::glue("inst/output/sims914raw/allsims_raw", i, ".csv"))
+    write_csv(summ_output, glue::glue("inst/output/sims914/allsims", i, ".csv"))
+  }
+
+  print(i)
+}
+
+
--- inst/scripts/simulation_exploration.R
+++ inst/scripts/simulation_exploration.R
@@ -1,16 +1,17 @@
 library(tidyverse)
 library(here)
-read_csv(here("inst", "output", "sim_deperr_lognormal_n50_gridloc.csv"))
 
-files <- list.files(path = here("inst", "output"), pattern = "*.csv",
-                    full.names = TRUE)
+files <- list.files(path = here("inst", "output", "sims914"), pattern = "*.csv", full.names = TRUE)
 
 combo_data <- purrr::map_df(files,
-                            ~read_csv(.x) %>% mutate(filename = .x)) %>%
-  separate(filename, into = c("junk", "error_dep", "dist", "n", "location"),
-           sep = "_") %>%
-  select(-junk)
-combo_data <- combo_data %>% mutate(sim = interaction(error_dep, dist, n, location))
+                            ~read_csv(.x) %>% mutate(filename = .x))
+
+# %>%
+#   separate(filename, into = c("junk", "error_dep", "dist", "n", "location"),
+#            sep = "_") %>%
+#   select(-junk)
+
+combo_data <- combo_data %>% mutate(sim = interaction(n, psill, resptype, gridded))
 
 ##
 ggplot(data = combo_data, aes(x = sim, y = rmspe, colour = approach)) +
@@ -27,17 +28,8 @@ summary(lm(rmspe ~ approach, data = combo_data_grts))
 ## evidence that normal distribution gives better estimators (makes sense)
 ## evidence that n of 50 gives worse estimators (makes sense)
 
-summary(lm(rmspe ~ approach + error_dep + dist + n + location,
+summary(lm(rmspe ~ approach + resptype + psill + n + gridded,
            data = combo_data_grts))
-
-summary(lm(rmspe ~ approach + error_dep*approach + dist* approach + n * approach + location * approach, data = combo_data_grts))
-
-
-## checking other things
-summary(lm(med_ci_len ~ approach, data = combo_data_grts))
-summary(lm(med_ci_len ~ approach + error_dep + dist + n + location,
-           data = combo_data_grts))
-
 
 
 ## visually explore the two approaches
@@ -46,7 +38,7 @@ ggplot(data = combo_data_grts, aes(x = sim, y = rmspe, colour = approach)) +
   geom_point() +
   coord_flip()
 
-dep_only <- combo_data_grts %>% filter(error_dep == "deperr")
+dep_only <- combo_data_grts %>% filter(psill == max(psill))
 dep_long <- dep_only %>%
   pivot_wider(names_from = approach, values_from = rmspe, sim) %>%
   mutate(perc = 100 * (`Design GRTS` - `Model GRTS`) / `Model GRTS`)
@@ -62,8 +54,9 @@ combo_perc <- combo_data_grts %>%
 ggplot(data = combo_perc, aes(x = fct_reorder(sim, perc), y = perc)) +
   geom_point() +
   coord_flip()
+
 ## investigate discrepancy in grid vs random
-combo_data_grts %>% filter(error_dep == "deperr", dist == "lognormal", n == "n150")
+combo_data_grts %>% filter(psill == max(psill), resptype == "lognormal", n == 200)
 
 combo_perc <- combo_data_grts %>%
   pivot_wider(names_from = approach, values_from = medae, sim) %>%
@@ -81,16 +74,16 @@ ggplot(data = combo_data_grts, aes(x = sim, y = coverage, colour = approach)) +
 
 ## all
 
-grid_df <- read_csv(here("inst", "all_output", "sim_deperr_lognormalbig_n150_gridloc.csv"))
-grid_df$location <- "grid"
-rand_df <- read_csv(here("inst", "all_output", "sim_deperr_lognormalbig_n150_randloc.csv"))
-rand_df$location <- "rand"
-
-both_df <- bind_rows(grid_df, rand_df)
-both_df %>% group_by(location) %>%
-  summarise(max(true_mean))
-ggplot(data = both_df, aes(x = true_mean)) +
-  geom_histogram(bins = 15) +
-  facet_wrap(~ location)
-
-
+# grid_df <- read_csv(here("inst", "all_output", "sim_deperr_lognormalbig_n150_gridloc.csv"))
+# grid_df$location <- "grid"
+# rand_df <- read_csv(here("inst", "all_output", "sim_deperr_lognormalbig_n150_randloc.csv"))
+# rand_df$location <- "rand"
+#
+# both_df <- bind_rows(grid_df, rand_df)
+# both_df %>% group_by(location) %>%
+#   summarise(max(true_mean))
+# ggplot(data = both_df, aes(x = true_mean)) +
+#   geom_histogram(bins = 15) +
+#   facet_wrap(~ location)
+#
+#
